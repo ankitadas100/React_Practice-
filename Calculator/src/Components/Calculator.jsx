d30e72a77c5504fa81597b2c6a3280bc7a92bce7
ankitadas100 <ankitadas2308@gmail.com>
2026-08-12 01:34:35 +0530

    Add calculator UI and operator state

--- Calculator/src/Components/Calculator.jsx
+++ Calculator/src/Components/Calculator.jsx
@@ -1,34 +1,38 @@
+import { useState } from "react";
 import "./Calculator.css"
 function Calculator() {
+    const [display, SetDisplay] = useState("0");
+    const[operator,SetOperator]=useState(" ");
     return (
         < div className="calculator">
             <div className="header">Calculator</div>
-            <div className="display">0</div>
+            <div className="display">{display} {operator}</div>
+            {/* <div className="display">{operator}</div> */}
             <div className="button">
-                
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>+</button>
-                
-                
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>-</button>
-                
-                
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>×</button>
-               
-               
-                    <button>C</button>
-                    <button>0</button>
-                    <button>=</button>
-                    <button>÷</button>
-                
+
+                <button onClick={() => SetDisplay("7")}>7</button>
+                <button onClick={() => SetDisplay("8")}>8</button>
+                <button onClick={() => SetDisplay("9")}>9</button>
+                <button onClick={()=>SetOperator("+")}>+</button>
+
+                <button onClick={() => SetDisplay("4")}>4</button>
+                <button onClick={() => SetDisplay("5")}>5</button>
+                <button onClick ={() => SetDisplay("6")}>6</button>
+               <button onClick={()=>SetOperator("-")}>-</button>
+
+
+                <button onClick={() => SetDisplay("1")}>1</button>
+                <button onClick={() => SetDisplay("2")}>2</button>
+                <button onClick={() => SetDisplay("3")}>3</button>
+                <button onClick={()=>SetOperator("x")}>x</button>
+
+
+
+                <button onClick={()=>SetOperator("C")}>C</button>
+                <button onClick={() => SetDisplay("0")}>0</button>
+                <button onClick={()=>SetOperator("=")}>=</button>
+                <button onClick={()=>SetOperator("÷")}>÷</button>
+
             </div>
 
         </div>

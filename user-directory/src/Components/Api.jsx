a911a1f59d58857e847424d01e39f007e1830e55
ankitadas100 <ankitadas2308@gmail.com>
2026-08-15 00:18:45 +0530

    update

--- user-directory/src/Components/Api.jsx
+++ user-directory/src/Components/Api.jsx
@@ -1,4 +1,9 @@
 import { useState, useEffect } from "react";
+import { FiUser } from "react-icons/fi";
+import { FaUserLarge } from "react-icons/fa6";
+import { MdMarkEmailRead } from "react-icons/md";
+import { PiCityBold } from "react-icons/pi";
+import "./Api.css"
 function Api() {
     const [user, SetUser] = useState([])
     const [load, SetLoad] = useState(false)
@@ -23,20 +28,49 @@ function Api() {
     useEffect(() => {
         getUsers();
     }, []);
-    return (
-        <div>
-            {load && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            {user.map((oneUser) => (
-                <div key={oneUser.id}>
-                    <h3>{oneUser.name}</h3>
-                    <p>Username: {oneUser.username}</p>
-                    <p>Email: {oneUser.email}</p>
-                    <p>City: {oneUser.address.city}</p>
-                </div>
-            ))}
+   return (
+    <div className="user-container">
 
-        </div>
-    )
+        {load && <p>Loading...</p>}
+
+        {error && <p>{error}</p>}
+
+        {user.map((oneUser) => (
+            <div className="user-card" key={oneUser.id}>
+
+                <div className="user-subcard">
+                    <div className="user_mainicon">
+                   <FiUser className="user-icon" />  {oneUser.name}
+                   </div>
+                </div>
+
+                <div className="user-information">
+
+                    <p>
+                        <FaUserLarge className="user-icon2" />
+                        <span className="label">Username:</span>{" "}
+                       <span className="value" >{oneUser.username} </span>
+                    </p>
+
+                    <p>
+                        <MdMarkEmailRead  className="user-icon3"/>
+                        <span className="label">Email:</span>{" "}
+                       <span className="value" >{oneUser.email}</span>
+                    </p>
+
+                    <p>
+                        <PiCityBold  className="user-icon4"/>
+                        <span className="label">City:</span>{" "}
+                        <span className="value">{oneUser.address.city}</span>
+                    </p>
+
+                </div>
+
+            </div>
+        ))}
+
+    </div>
+);
 }
+
 export default Api;
